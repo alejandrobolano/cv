5b76cabb964aa43a34c561cb1bf8f780369baf5a
Alejandro <elbola.cuba1992@gmail.com>
2020-09-12 19:10:40 +0200

    Add translation

---
+++ src/app/core/translate/translate.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {TranslateService} from '@ngx-translate/core';
+import {HttpClient} from '@angular/common/http';
+import {TranslateWrapperService} from '../service/translate-wrapper.service';
+
+@Component({
+  selector: 'ambm-core',
+  templateUrl: './translate.component.html'
+})
+export class TranslateComponent implements OnInit {
+  language;
+
+  constructor(public translate: TranslateService,
+              public translateWrapperService: TranslateWrapperService) {
+    this.useLanguage();
+  }
+
+  ngOnInit(): void {
+    this.useLanguage();
+  }
+
+  private useLanguage(): void {
+    this.translateWrapperService.languageChange.subscribe(
+      value => {
+        this.language = value;
+        this.translate.setDefaultLang(this.language);
+        this.translate.use(this.language);
+      }
+    );
+  }
+
+}
