01c1dea702111d51525e026fe43cc07f78b18b93
Alejandro <elbola.cuba1992@gmail.com>
2020-10-02 17:38:17 +0200

    Add Cookie service

---
+++ src/app/core/service/cookies.service.ts
@@ -0,0 +1,40 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CookiesService {
+
+  constructor() {
+  }
+
+  getCookie(name: string): string {
+    const ca: Array<string> = document.cookie.split(';');
+    const caLen: number = ca.length;
+    const cookieName = `${name}=`;
+    let c: string;
+
+    for (let i = 0; i < caLen; i += 1) {
+      c = ca[i].replace(/^\s+/g, '');
+      if (c.indexOf(cookieName) === 0) {
+        return c.substring(cookieName.length, c.length);
+      }
+    }
+    return '';
+  }
+
+  deleteCookie(name): void {
+    this.setCookie(name, '', -1);
+  }
+
+  setCookie(name: string, value: string, expireDays: number, path: string = ''): void {
+    const date: Date = new Date();
+    date.setTime(date.getTime() + expireDays * 24 * 60 * 60 * 1000);
+    const expires = `expires=${date.toUTCString()}`;
+    const cPath: string = path ? `; path=${path}` : '';
+    document.cookie = `${name}=${value}; ${expires}${cPath}`;
+  }
+
+
+
+}
