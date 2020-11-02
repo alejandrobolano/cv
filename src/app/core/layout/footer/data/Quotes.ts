74a1ffa6e1188c5dce645b6b39ab76249fb35417
Alejandro <alejandro.bolano@col.vueling.com>
2020-11-02 17:43:26 +0100

    Add dynamic quotes in footer nav

---
+++ src/app/core/layout/footer/data/Quotes.ts
@@ -0,0 +1,14 @@
+import {IQuote} from './IQuote';
+
+export const QUOTES: IQuote[] = [
+  {
+    id: 1,
+    author: 'Steve Jobs',
+    quote: 'I\'m convinced that about half of what separates successful entrepreneurs from the non-successful ones is pure perseverance'
+  },
+  {
+    id: 2,
+    author: 'Mark Twain',
+    quote: 'The best way to cheer yourself up is to try to cheer somebody else up'
+  }
+];
--- src/app/core/layout/footer/footer.component.ts
+++ src/app/core/layout/footer/footer.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {QUOTES} from './data/Quotes';
+import {IQuote} from './data/IQuote';
+import {WindowResizeService} from '../../service/window-resize.service';
 
 @Component({
   selector: 'ambm-footer',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
   title = 'Alejandro M. Bolaño M.';
-  constructor() {}
+  quote: IQuote;
 
-  ngOnInit(): void {
+  constructor(private windowResize: WindowResizeService) {
   }
 
+  get isMobile(): boolean {
+    return this.windowResize.IsMobile;
+  }
+
+
+  private static getRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  ngOnInit(): void {
+    this.quote = this.getQuote();
+  }
+
+  getQuote(): IQuote {
+    const quotes = QUOTES;
+    const index = FooterComponent.getRandomInt(0, quotes.length);
+    return quotes[index];
+  }
 }
