04b6e92b4a3c39d3aa0a40d46a867e66c3932d8c
Alejandro <alejandro.bolano@col.vueling.com>
2020-11-02 17:47:07 +0100

    Update translate for obtain value of cookie

--- src/app/core/service/translate-wrapper.service.ts
+++ src/app/core/service/translate-wrapper.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {BehaviorSubject} from 'rxjs';
 import {TranslateService} from '@ngx-translate/core';
 import {LanguageEnum} from '../enum/LanguageEnum';
+import {CookiesService} from './cookies.service';
 
 @Injectable({
   providedIn: 'root'
@@ -10,22 +11,40 @@ export class TranslateWrapperService {
   language$ = new BehaviorSubject<string>(LanguageEnum.ENGLISH);
   languageChange = this.language$.asObservable();
 
-  constructor(public translate: TranslateService) {
+  constructor(public translate: TranslateService,
+              public cookies: CookiesService) {
     this.useLanguage();
   }
 
   private useLanguage(): void{
-    const browserLang = this.translate.getBrowserLang();
-    this.language = browserLang.match(/en|es|ca/) ? browserLang : this.language;
+    this.language = this.getLanguageOfCookie();
     this.translate.use(this.language);
     this.translate.setDefaultLang(this.language);
   }
 
   set language(value: string) {
     this.language$.next(value);
+    this.setLanguageOfCookie(value);
   }
 
   get language(): string {
     return this.language$.getValue();
+  }
+
+  getLanguageOfCookie(): string{
+    let language = this.cookies.getCookie('lang');
+    if (!language) {
+      const browserLang = this.translate.getBrowserLang();
+      language = browserLang.match(/en|es|ca/) ? browserLang : this.language;
+      this.setLanguageOfCookie(language);
+    }
+    return language;
+  }
+
+  setLanguageOfCookie(language: string): void {
+    this.cookies.setCookie('lang',
+      language,
+      7,
+      '/');
   }
 }
