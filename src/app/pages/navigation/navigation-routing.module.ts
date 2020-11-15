f74123475982b4983d43471c270d57473b75ead3
Alejandro <alejandro.bolano@col.vueling.com>
2020-11-15 12:11:40 +0100

    Temporal: redirect portfolio to page under construction

--- src/app/pages/navigation/navigation-routing.module.ts
+++ src/app/pages/navigation/navigation-routing.module.ts
@@ -4,13 +4,14 @@ import { NavigationComponent } from './navigation.component';
 import { HomeComponent } from './home/home.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import {ContactComponent} from './contact/contact.component';
+import {PageUnderConstructionComponent} from '../../core/layout/page-under-construction/page-under-construction.component';
 
 const routes: Routes = [
   { path: '', component: NavigationComponent,
   children: [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent},
-    { path: 'portfolio', component: PortfolioComponent },
+    { path: 'portfolio', component: PageUnderConstructionComponent },
     { path: 'cv', pathMatch: 'full', redirectTo: 'about-me'},
     { path: 'curriculum', pathMatch: 'full', redirectTo: 'about-me'},
     { path: 'about-me', loadChildren: () => import('./about-me/about-me.module').then(m => m.AboutMeModule)},
