d1000f891a933824715aca50f3780b46ba909835
Alejandro <alejandro.bolano@col.vueling.com>
2021-02-01 20:13:42 +0100

    Add translations to home´s component

--- src/app/pages/navigation/home/home.component.ts
+++ src/app/pages/navigation/home/home.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 import {TranslateWrapperService} from '../../../core/service/translate-wrapper.service';
 import {TranslateComponent} from '../../../core/translate/translate.component';
+import {MySocialNetworksEnum} from '../../../core/enum/my-social-networks.enum';
+import {LinksEnum} from './enum/links.enum';
 
 @Component({
   selector: 'ambm-home',
@@ -11,12 +13,17 @@ import {TranslateComponent} from '../../../core/translate/translate.component';
 export class HomeComponent extends TranslateComponent implements OnInit {
   language;
   image = '/assets/img/code-banner.jpg';
+  thisRepoGitHubLink = LinksEnum.ThisRepoGitHub;
+  angularCliLink = LinksEnum.AngularCli;
+  firebaseLink = LinksEnum.Firebase;
+  antDesignLink = LinksEnum.AntDesign;
 
   constructor(public translate: TranslateService,
               public translateWrapperService: TranslateWrapperService) {
     super(translate, translateWrapperService);
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
 }
