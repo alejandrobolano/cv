d7765476f4030f2913e7dd0998783a9ea0f07452
Alejandro Bolaño <elbola.cuba1992@gmail.com>
2023-02-20 15:26:12 +0100

    feat: Improvement about styles.

--- src/app/pages/navigation/home/enum/links.enum.ts
+++ src/app/pages/navigation/home/enum/links.enum.ts
@@ -3,5 +3,5 @@ export enum LinksEnum {
   Firebase = 'https://firebase.google.com/docs?authuser=0',
   ThisRepoGitHub = 'https://github.com/alejandrobolano/cv',
   AntDesign = 'https://ng.ant.design/',
-  ImageProfile = 'https://media.licdn.com/dms/image/D4E16AQG49l7Q8r7nDQ/profile-displaybackgroundimage-shrink_350_1400/0/1674135882882?e=1681344000&v=beta&t=AZiuvWAXlzaQjLevhvzlDAEi24uhH2eh3ggtK7BtvXs'
+  BannerProfile = 'https://media.licdn.com/dms/image/D4E16AQG49l7Q8r7nDQ/profile-displaybackgroundimage-shrink_350_1400/0/1674135882882?e=1681344000&v=beta&t=AZiuvWAXlzaQjLevhvzlDAEi24uhH2eh3ggtK7BtvXs'
 }
